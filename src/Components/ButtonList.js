241731a2eafd315a93af315a4d667e9b3f52cb9a
mdimran1409036 <mdimran1409036@gmail.com>
2023-02-27 01:37:50 +0600

    sidebar data added

--- src/Components/ButtonList.js
+++ src/Components/ButtonList.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdOutlineNavigateNext } from "react-icons/md";
+
 import Button from "./Button";
 
 function ButtonList({ loading, error, data }) {
+    const [count, setCount] = useState(0);
     if (loading) {
         return (
             <div className="m-4 flex gap-4 flex-wrap animate-pulse">
@@ -18,10 +21,35 @@ function ButtonList({ loading, error, data }) {
         return <p>{error.message}</p>;
     }
     return (
-        <div className="m-4 flex gap-4 flex-wrap transition-all">
-            {data?.items?.slice(0, 12).map((item) => (
+        <div className="m-4 flex gap-4 flex-wrap items-center transition-all">
+            <MdOutlineNavigateNext
+                className={`rotate-180 ${
+                    count < 9 ? "cursor-not-allowed" : " cursor-pointer"
+                }`}
+                size={25}
+                onClick={() => {
+                    if (count >= 9) {
+                        setCount((prev) => prev - 10);
+                    }
+                }}
+            />
+
+            {data?.items?.slice(count, count + 10).map((item) => (
                 <Button key={item.id} text={item?.snippet?.title} />
             ))}
+            <MdOutlineNavigateNext
+                onClick={() => {
+                    if (count + 10 < data.items.length) {
+                        setCount((prev) => prev + 10);
+                    }
+                }}
+                className={`${
+                    count + 10 > data.items.length
+                        ? "cursor-not-allowed"
+                        : " cursor-pointer"
+                }`}
+                size={25}
+            />
         </div>
     );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,25 +1,62 @@
 import React from "react";
-
+import {
+    MdHistory,
+    MdHomeFilled,
+    MdOutlineOndemandVideo,
+    MdOutlineWatchLater,
+    MdSubscriptions,
+    MdSwitchVideo,
+    MdVideoLibrary,
+} from "react-icons/md";
+import { FaFreeCodeCamp, FaGithubAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 function Sidebar() {
     return (
         <>
             <ul className="py-2">
-                <li>Home</li>
-                <li>Shorts</li>
-                <li>Subscriptions</li>
+                <Link to="/">
+                    <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 py-1 mr-2 rounded">
+                        <MdHomeFilled size={20} /> <span>Home</span>
+                    </li>
+                </Link>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 mr-2 py-1 rounded">
+                    <MdSwitchVideo size={20} /> <span>Shorts</span>
+                </li>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 mr-2 py-1 rounded">
+                    <MdSubscriptions size={20} /> <span>Subscriptions</span>
+                </li>
             </ul>
             <hr />
             <ul className="py-2">
-                <li>Library</li>
-                <li>History</li>
-                <li>Your Videos</li>
-                <li>Watch Later</li>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 py-1 mr-2 rounded">
+                    <MdVideoLibrary size={20} /> <span>Library</span>
+                </li>
+
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 py-1 mr-2 rounded">
+                    <MdHistory size={20} /> <span>Library</span>
+                </li>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 py-1 mr-2 rounded">
+                    <MdOutlineOndemandVideo size={20} />{" "}
+                    <span>Your videos</span>
+                </li>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 py-1 mr-2 rounded">
+                    <MdOutlineWatchLater size={20} /> <span>Watch later</span>
+                </li>
             </ul>
             <h2 className="py-2 font-bold ">Subscriptions</h2>
             <ul>
-                <li>Free code camp</li>
-                <li>Akshay saini</li>
-                <li>Github</li>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 mr-2 py-1 rounded">
+                    <div className="border p-1 rounded-full">
+                        <FaFreeCodeCamp />
+                    </div>
+                    <span>Free code camp</span>
+                </li>
+                <li className="cursor-pointer flex items-center space-x-4 hover:bg-gray-100 px-2 mr-2 py-1 rounded">
+                    <div className="border p-1 rounded-full">
+                        <FaGithubAlt />
+                    </div>
+                    <span>Github</span>
+                </li>
             </ul>
         </>
     );
