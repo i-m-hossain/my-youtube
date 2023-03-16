03acfd42cafa0a934b6db5ad88a522050ff13d1f
imranhAltersense <114974565+imranhAltersense@users.noreply.github.com>
2023-03-16 18:36:02 +0600

    live video card issue resolved

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,67 +10,55 @@ import VideoCard from "./VideoCard";
 import VideoContainer from "./VideoContainer";
 
 const Home = () => {
-    const [loading, error, data] = useFetchData(YOUTUBE_VIDEO_LIST_API);
-    const [catLoading, catError, catData] = useFetchData(YOUTUBE_CATEGORIES);
-    const buttonVideoList = useSelector(
-        (store) => store.buttonListVideos.buttonListVideos
-    );
+  const [loading, error, data] = useFetchData(YOUTUBE_VIDEO_LIST_API);
+  const [catLoading, catError, catData] = useFetchData(YOUTUBE_CATEGORIES);
+  const buttonVideoList = useSelector(
+    (store) => store.buttonListVideos.buttonListVideos
+  );
 
-    if (loading) {
-        return (
-            <Body>
-                <VideoContainer>
-                    {new Array(12).fill("").map((item, i) => (
-                        <Shimmer key={i} />
-                    ))}
-                </VideoContainer>
-            </Body>
-        );
-    }
-    if (error) {
-        return <p>{error.message}</p>;
-    }
-    console.log(buttonVideoList);
+  if (loading) {
     return (
-        <>
-            <Body>
-                <ButtonList
-                    loading={catLoading}
-                    error={catError}
-                    data={catData}
-                />
-                <VideoContainer>
-                    <LiveVideoCard
-                        video={
-                            buttonVideoList?.items
-                                ? buttonVideoList?.items[0]
-                                : data.items[0]
-                        }
-                    />
-
-                    {buttonVideoList?.items
-                        ? buttonVideoList?.items
-                              .slice(1)
-                              .map((video, i) => (
-                                  <VideoCard
-                                      key={i}
-                                      video={video}
-                                      id={video.id.videoId}
-                                  />
-                              ))
-                        : data?.items
-                              .slice(1)
-                              .map((video, i) => (
-                                  <VideoCard
-                                      key={i}
-                                      video={video}
-                                      id={video.id}
-                                  />
-                              ))}
-                </VideoContainer>
-            </Body>
-        </>
+      <Body>
+        <VideoContainer>
+          {new Array(12).fill("").map((item, i) => (
+            <Shimmer key={i} />
+          ))}
+        </VideoContainer>
+      </Body>
     );
+  }
+  if (error) {
+    return <p>{error.message}</p>;
+  }
+  console.log(buttonVideoList);
+  return (
+    <>
+      <Body>
+        <ButtonList loading={catLoading} error={catError} data={catData} />
+        <VideoContainer>
+          <LiveVideoCard
+            video={
+              buttonVideoList?.items
+                ? buttonVideoList?.items[0]
+                : data?.items[0]
+            }
+          />
+
+          {buttonVideoList?.items
+            ? buttonVideoList?.items
+                .slice(1)
+                .map((video, i) => (
+                  <VideoCard key={i} video={video} id={video.id.videoId} />
+                ))
+            : data?.items
+                .slice(1)
+                .map((video, i) => (
+                  <VideoCard key={i} video={video} id={video.id} />
+                ))}
+        </VideoContainer>
+      </Body>
+    </>
+  );
 };
 
 export default Home;
